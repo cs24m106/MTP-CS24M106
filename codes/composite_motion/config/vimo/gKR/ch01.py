f36e5c5ce52fea5171b15ed559b509ab92d38428
Logesh <cs24m106@iittp.ac.in>
2026-04-04 21:07:31 +0530

    vimo motions & configs organized

---
+++ codes/composite_motion/config/vimo/gKR/ch01.py
@@ -0,0 +1,16 @@
+env_cls = "ICCGANHumanoid"
+env_params = dict(
+    motion_file = "assets/motions/vimo/gKR/clips_ch01.yaml"
+)
+
+training_params = dict(
+    max_epochs = 10000,
+    save_interval = 500,
+    terminate_reward = -1
+)
+
+discriminators = {
+    "_/full": dict(
+        parent_link = None,
+    )
+}
